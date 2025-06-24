925b7444bf5f0576c9271508c8d5f4556d16da1c
Samrat Singh <samrat.singh@f1soft.com>
2025-06-24 18:24:52 +0545

    added a new module, and made changes to project structure

---
+++ reporting-and-analytics/src/test/java/com/example/reportingAndAnalytics/ReportingAndAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reportingAndAnalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportingAndAnalyticsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
